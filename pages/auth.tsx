845dee03b83ad6ef6429dd4ca383a0d2bd6dce4e
Devesh <deveshpachauri23@gmail.com>
2023-03-14 10:41:53 +0530

    backend

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,7 +5,25 @@ import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
-import { sign } from "crypto";
+import { NextPageContext } from "next";
+
+
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+  
+    if (session) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        }
+      }
+    }
+  
+    return {
+      props: {}
+    }
+  }
 
 const Auth = () =>{
     const router = useRouter();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
+import { NextPageContext } from "next"
+import { getSession, signOut } from "next-auth/react"
+
+
+export async function getServerSideProps(context:NextPageContext) {
+  const session = await getSession(context)
+
+  if(!session){
+    return{
+      redirect: {
+        callbackurl: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
   return (
     <>
     <h1 className="text-2xl text-green-500">Netflix-clone</h1>
+    <button className="h-10 w-10 bg-white" onClick={()=> signOut()}>Logout</button>
     </>
+
+    
   )
 }
